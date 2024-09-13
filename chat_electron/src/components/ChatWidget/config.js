990105de2491dde41e82ac9d3e413aa14538a21b
TLAMHutto <keatonhutto8@gmail.com>
2024-09-13 15:12:29 -0500

    v1

--- chat_electron/src/components/ChatWidget/config.js
+++
@@ -1,3 +0,0 @@
-export const colors = {
-    primary: "#55230e",
- };
--- chat_electron/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
-module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
