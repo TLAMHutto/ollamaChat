67a8858fd752b4a797dfd8e5b241596646ce1e52
TLAMHutto <keatonhutto8@gmail.com>
2024-09-17 01:01:49 -0500

    starts ollama on run

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-
 import sys
+import subprocess
 from PyQt6.QtWidgets import QApplication
 from chat_app import MinimalistChatApp
 
+# Use subprocess.Popen to run the executable without blocking
+subprocess.Popen([r"C:\Users\keaton\AppData\Local\Programs\Ollama\ollama app.exe"], shell=True)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
