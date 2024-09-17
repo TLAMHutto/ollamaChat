8aee51bd71b935ac11e7de2892df12e872c99004
TLAMHutto <keatonhutto8@gmail.com>
2024-09-17 02:08:35 -0500

    buttons change

--- chat_app.py
+++ chat_app.py
@@ -20,43 +20,53 @@ class MinimalistChatApp(QWidget):
         main_layout = QHBoxLayout()
         main_layout.setContentsMargins(0, 0, 0, 0)  # Remove margins
 
-        # Create small square button
         self.square_button = QPushButton(self)
         self.square_button.setFixedSize(35, 35)  # Set fixed size for square button
+        
+        self.chat_button = QPushButton(self)
+        self.chat_button.setFixedSize(35, 35)
+        
+        self.exit_button = QPushButton(self)
+        self.exit_button.setFixedSize(35, 35)
 
-        # Load the icon image
-        icon_pixmap = QPixmap('./zoom-scan.png')
-        self.square_button.setFixedSize(20, 20)
-        # Set a smaller size for the icon
-        icon_size = QSize(20, 20)  # Specify the desired size for the icon
-        self.square_button.setIcon(QIcon(icon_pixmap))
-        self.square_button.setIconSize(icon_size)  # Set the size of the icon
-
+        # Load the icon images
+        scan = QPixmap('./zoom-scan.png')
+        chat = QPixmap('./message-dots.png')
+        exit = QPixmap('./square-letter-x.png')
+        
+        # Configure square button
+        self.square_button.setIcon(QIcon(scan))
+        self.square_button.setIconSize(QSize(20, 20))  # Set the size of the icon
         self.square_button.setStyleSheet('background-color: lightgray; border: none;')  # Optional: set background color and remove border
         self.square_button.clicked.connect(self.open_ocr_window)  # Connect button click to open OCR window
+        
+        # Configure chat button
+        self.chat_button.setIcon(QIcon(chat))
+        self.chat_button.setIconSize(QSize(20, 20))  # Set the size of the icon
+        self.chat_button.setStyleSheet('background-color: lightgray; border: none;')  # Optional: set background color and remove border
+        self.chat_button.clicked.connect(self.toggle_chat_window)  # Connect button click to open chat window
+
+        # Configure exit button
+        self.exit_button.setIcon(QIcon(exit))
+        self.exit_button.setIconSize(QSize(20, 20))  # Set the size of the icon
+        self.exit_button.setStyleSheet('background-color: lightgray; border: none;')  # Optional: set background color and remove border
+        self.exit_button.clicked.connect(self.close)
+
+        # Add buttons to layout
+        main_layout = QHBoxLayout(self)
         main_layout.addWidget(self.square_button)
-
-        # Create chat window button
-        self.chat_button = QPushButton('Open Chat', self)
-        self.chat_button.clicked.connect(self.toggle_chat_window)
         main_layout.addWidget(self.chat_button)
-
-        # Create exit button
-        exit_button = QPushButton('Exit', self)
-        exit_button.clicked.connect(self.close)
-        main_layout.addWidget(exit_button)
-
-        self.setLayout(main_layout)
+        main_layout.addWidget(self.exit_button)
 
     def toggle_chat_window(self):
         if self.chat_window and self.chat_window.isVisible():
             self.chat_window.hide()
-            self.chat_button.setText('Open Chat')
+            
         else:
             if not self.chat_window:
                 self.chat_window = ChatWindow(self)
             self.chat_window.show()
-            self.chat_button.setText('Close Chat')
+            
 
     def open_ocr_window(self):
         # Run Tkinter window in a separate thread
